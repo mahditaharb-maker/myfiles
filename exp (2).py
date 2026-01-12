7a2ad88c5b2ce66fd54d3af8070f94da7d8d4135
mahditaharb-maker <mahditaharb@gmail.com>
2026-01-12 23:44:30 +0100

    Initial commit

--- exp (2).py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-from fractions import Fraction
-
-mod = 65537 # Modular base (can adjust)
-terms = 100  # Number of terms to calculate
-approx_e = sum(Fraction(1, factorial(n)) for n in range(terms)) % mod
-
-print(f"Approximation of e as fraction: {approx_e}")
--- h.py
+++
@@ -1,6 +0,0 @@
-from proveit import A
-from proveit.logic import Not, Implies
-from proveit.logic.boolean.negation.theorems import fromDoubleNegation
-
-# Display the theorem
-fromDoubleNegation
